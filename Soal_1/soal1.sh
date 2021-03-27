036c0b9a4b289d9eb3257961cf0ad12a18208cb3
yogarsdna <74660281+yogarsdna@users.noreply.github.com>
2021-03-27 18:54:57 +0700

    Update soal1.sh

--- Soal_1/soal1.sh
+++ Soal_1/soal1.sh
@@ -13,3 +13,14 @@ echo "Show the number of occurrences of the error for each user:"
 cat syslog.log|grep ERROR|cut -d "(" -f2|cut -d ")" -f1|sort|uniq -c
 echo "Show the number of occurrences of the info for each user:"
 cat syslog.log|grep INFO|cut -d "(" -f2|cut -d ")" -f1|sort|uniq -c
+
+#1D Error Message
+echo "error_message.csv have been made"
+infoerror:`cat syslog.log|grep -o 'ERROR.*'|cut -d " " -f2-|cut -d "(" -f1|sort|uniq -c|sort -nr`
+echo "$infoerror"|
+while read checkerror
+do
+  totalerror=`echo $checkerror|cut -d " " -f1`
+  nameerror=`echo $checkerror|cut -d " " -f2-`
+  echo "$nameerror,$totalerror"
+done|sed '1 i\Error,Count' > error_message.csv
