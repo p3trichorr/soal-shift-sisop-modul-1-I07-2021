5bd73599cbbb1c833b8199a605505bbdef771d79
yogarsdna <74660281+yogarsdna@users.noreply.github.com>
2021-03-27 18:27:08 +0700

    Update soal1.sh

--- Soal_1/soal1.sh
+++ Soal_1/soal1.sh
@@ -6,7 +6,7 @@ cat syslog.log|cut -d ":" -f4
 
 #1B Show all error messages with the number of occurrences
 echo "Show all error messages with the number of occurrences:"
-cat syslog.log|grep ERROR|cut -d ":" -f4|cut -d "(" -f1|sort|uniq -c
+cat syslog.log|grep -o 'ERROR.*'|cut -d " " -f2-|cut -d "(" -f1|sort|uniq -c| sort -nr
 
 #1C Show the number of occurrences of the error and info for each user
 echo "Show the number of occurrences of the error for each user:"
