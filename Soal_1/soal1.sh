15be97e4afd093df56053812d56544108e63ba81
yogarsdna <74660281+yogarsdna@users.noreply.github.com>
2021-04-01 18:41:10 +0700

    Update soal1.sh

--- Soal_1/soal1.sh
+++ Soal_1/soal1.sh
@@ -16,7 +16,7 @@ cat syslog.log|grep INFO|cut -d "(" -f2|cut -d ")" -f1|sort|uniq -c
 
 #1D Error Message
 echo "error_message.csv have been made"
-infoerror:`cat syslog.log|grep -o 'ERROR.*'|cut -d " " -f2-|cut -d "(" -f1|sort|uniq -c|sort -nr`
+infoerror=`cat syslog.log|grep -o 'ERROR.*'|cut -d " " -f2-|cut -d "(" -f1|sort|uniq -c|sort -nr`
 echo "$infoerror"|
 while read checkerror
 do
@@ -24,3 +24,14 @@ do
   nameerror=`echo $checkerror|cut -d " " -f2-`
   echo "$nameerror,$totalerror"
 done|sed '1 i\Error,Count' > error_message.csv
+
+#1E User Statistic
+echo "user_statistic.csv have been made"
+username=`cat syslog.log|cut -d "(" -f2|cut -d ")" -f1|sort|uniq`
+echo "$username"|
+while read user
+do
+  totalinfo=`cat syslog.log|grep -o "INFO.*($user)"| wc-l`
+  totalerror=`cat syslog.log|grep -o "ERROR.*($user)"| wc-l`
+  echo "$user,$totalinfo,$totalerror"
+done|sed '1 i\Username, INFO, ERROR' > user_statistic.csv
